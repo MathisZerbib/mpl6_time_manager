0ed2625756ebc5d307597195e2ef2d6dbb4f849e
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2022-11-03 16:15:23 +0100

    hide nav bar

--- front/src/components/nav/NavBar.vue
+++ front/src/components/nav/NavBar.vue
@@ -4,28 +4,13 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <!-- <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button> -->
-
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <RouterLink class="nav-link" to="/">Home</RouterLink>
-          <!-- <li class="navbar-nav">
-            <RouterLink class="nav-link" to="/userboard">User</RouterLink>
-          </li> -->
         </ul>
       </div>
-    </nav>
+    </nav> -->
   </header>
 
   <RouterView />
