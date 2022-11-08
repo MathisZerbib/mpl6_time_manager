a0140da64cb0d24411671c56246ce0a3f71d4658
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2022-11-09 00:58:44 +0100

    added guards + start axios login. form

--- front/src/components/form/FormLogin.vue
+++ front/src/components/form/FormLogin.vue
@@ -11,13 +11,13 @@
           <h2 class="login-title">Se Connecter</h2>
           <form>
             <div class="form-group">
-              <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+              <input  v-model="email" type="email" name="email" id="email" class="form-control" placeholder="Email">
             </div>
             <div class="form-group mb-3">
-              <input type="password" name="password" id="password" class="form-control mb-5" placeholder="Password">
+              <input v-model="password_hash" type="password" name="password" id="password" class="form-control mb-5" placeholder="Password">
             </div>
             <div class="d-flex justify-content-center  align-items-center mb-5">
-              <input name="login" id="login" class="btn-primary login-btn" type="button" value="Connexion">
+              <input @click="logUser" name="login" id="login" class="btn-primary login-btn" type="button" value="Connexion">
             </div>
           </form>           
           <p class="login-wrapper-footer-text">Vous n'avez pas de compte ? &nbsp;<a href="/register" class="text-reset">Enregistrez-vous ici</a></p>
@@ -29,14 +29,53 @@
 
 
 <script>
+import axios from "axios";
 
 export default {
   name: "FormLogin",
   data() {
     return {
-      
-
+      form: {
+        email: "",
+        password_hash: "",
+      },
     };
+  },
+  methods: {
+    logUser: async function () {
+      await axios
+        .get(
+          "http://" + "127.0.0.1" + ":4000/api/users"
+          //,
+          // {
+          //   user: {
+          //     email: this.email,
+          //     password_hash: this.password_hash,
+          //   },
+          // }
+        )
+        .then((response) => {
+          setTimeout(() => {
+            console.log("user has been loged", response.data, "status", response.status)
+            this.$router.push('dashboard') 
+          }, 500);
+        })
+        .catch(function (error) {
+          // error
+          console.log(error);
+        });
+    },
+    // onReset(event) {
+    //   event.preventDefault();
+    //   // Reset our form values
+    //   this.form.email = "";
+    //   this.form.name = "";
+    //   // Trick to reset/clear native browser form validation state
+    //   this.show = false;
+    //   this.$nextTick(() => {
+    //     this.show = true;
+    //   });
+    // },
   },
 };
 </script>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,6 +3,26 @@ import HomeView from "../views/HomeView.vue";
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 
+function guardAdmin(to, from, next)
+{
+ var isAuthenticated= false;
+//this is just an example. You will have to find a better or 
+// centralised way to handle you localstorage data handling 
+if(localStorage.getItem('role') == "admin")
+  isAuthenticated = true;
+ else
+  isAuthenticated= false;
+ if(isAuthenticated)
+ {
+  next(); // allow to enter route
+ } 
+ else
+ {
+  next('/'); // go to '/login';
+ }
+}
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +40,7 @@ const router = createRouter({
     {
       path: "/dashboard",
       name: "dashboard",
+      beforeEnter : guardAdmin,
       component: HomeView,
     },
     // {
@@ -49,8 +70,8 @@ const router = createRouter({
     //   component: () => import("../components/working_times/workingTimes.vue"),
     // },
   ],
-  linkActiveClass: "active-link",
-  linkExactActiveClass: "exact-active-link",
+  // linkActiveClass: "active-link",
+  // linkExactActiveClass: "exact-active-link",
 });
 
 export default router;
