f08e677eccd6615c456aa8ceb9a91a2192128468
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2022-11-10 17:25:22 +0100

    Update vue3 pie chart

--- front/src/components/charts/PieChart.vue
+++ front/src/components/charts/PieChart.vue
@@ -1,63 +1,56 @@
 <template>
-      <div class="d-flex justify-content-around my-3">
+  <div class="d-flex justify-content-around my-3">
 
-  <div class="card rounded shadow">
-    <h5 class="text-center my-3">Recap & Offset</h5>
-    <div class="card-body">
-      <Doughnut
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        :width="300"
-        :height="300"
-      />
-    </div>
+<div class="card rounded shadow">
+<h5 class="text-center my-3">Recap & Offset</h5>
+<div class="card-body">
+  <Doughnut
+    :chart-options="chart.chartOptions"
+    :chart-data="chart.chartData"
+    :width="300"
+    :height="300"
+  />
+</div>
+</div>
   </div>
-      </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted, computed, reactive, onUnmounted} from 'vue'
+
 import { Doughnut } from "vue-chartjs";
 import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  ArcElement,
-  CategoryScale,
+Chart as ChartJS,
+Title,
+Tooltip,
+Legend,
+ArcElement,
+CategoryScale,
 } from "chart.js";
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 // const TIME_LIMIT = 60;
-export default {
-  name: "PieChart",
-  components: {
-    Doughnut,
-  },
-  data() {
-    return {
-      chartData: {
-        labels: ["Temps travaillé", "Temps restant"],
-        datasets: [
-          {
-            backgroundColor: ["#198755", "#D33F49"],
-            data: [20, 40],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: false,
-        maintainAspectRatio: false,
-        cutout: 0,
-        plugins: {
-          legend: {
-            display: false,
-          },
-        },
-      },
-    };
-  },
-  mounted() {},
 
-  methods: {},
-};
+
+const chart = reactive ( {
+chartData: {
+    labels: ["Temps travaillé", "Temps restant"],
+    datasets: [
+      {
+        backgroundColor: ["#198755", "#D33F49"],
+        data: [20, 40],
+      },
+    ],
+  },
+  chartOptions: {
+    responsive: false,
+    maintainAspectRatio: false,
+    cutout: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
 </script>
