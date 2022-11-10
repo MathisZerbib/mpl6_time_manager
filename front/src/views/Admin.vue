7c0041a574a71da3f9f4180c836629e0e116df10
caillau-thomas <thomas.caillau@gmail.com>
2022-11-10 17:43:30 +0100

    Add some panels to some pages

--- front/src/views/Admin.vue
+++ front/src/views/Admin.vue
@@ -3,19 +3,27 @@ import UserTable from "../components/user/UserTable.vue";
 import WorkingTime from "../components/form_working_time/FormWorkingTime.vue";
 import WorkingTimesTable from "../components/working_times/WorkingTimesTable.vue";
 import FormUser from "../components/form_user/FormUser.vue";
+import BarChart from "../components/charts/BarChart.vue";
+import ChartClock from "../components/charts/ChartClock.vue";
+import PieChart from "../components/charts/PieChart.vue";
 
 </script>
 
 <template>
   <main class="container">
     <h1 class="text-center my-5">Time Manager</h1>
-      <div class="card d-flex flex-sm-column flex-md-column justify-content-around flex-column my-3">
+    <div class="card d-flex flex-sm-column flex-md-row justify-content-around flex-column my-3">
+      <ChartClock />
+      <BarChart />
+      <PieChart />
+    </div>
+    <div class="card d-flex flex-sm-column flex-md-column justify-content-around flex-column my-3">
       <WorkingTimesTable />
       <UserTable />
     </div>
     <div class="card d-flex flex-sm-column flex-md-column justify-content-around flex-column my-3">
-      <WorkingTime/>
-      <FormUser/>
+      <WorkingTime />
+      <FormUser />
     </div>
   </main>
 </template>
--- front/src/views/ManagerDashboard.vue
+++ front/src/views/ManagerDashboard.vue
@@ -4,6 +4,7 @@ import BarChart from "../components/charts/BarChart.vue";
 import ChartClock from "../components/charts/ChartClock.vue";
 import PieChart from "../components/charts/PieChart.vue";
 import WorkingTimesTable from "../components/working_times/WorkingTimesTable.vue";
+import WorkingTime from "../components/form_working_time/FormWorkingTime.vue";
 
 </script>
 
@@ -18,6 +19,9 @@ import WorkingTimesTable from "../components/working_times/WorkingTimesTable.vue
         <div class="card d-flex flex-sm-column flex-md-column justify-content-around flex-column my-3">
             <WorkingTimesTable />
             <UserTable />
+        </div>
+        <div class="card d-flex flex-sm-column flex-md-column justify-content-around flex-column my-3">
+            <WorkingTime />
         </div>
     </main>
 </template>
